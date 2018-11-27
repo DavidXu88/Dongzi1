181fecc408329fd47760ac1aedfe9ea9c2758806
DavidXu88 <5447282@qq.com>
2018-11-27 10:55:04 +0800

    ff

--- src/main/java/com/isoftstone/sample/Dongzi1Application.java
+++ src/main/java/com/isoftstone/sample/Dongzi1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Dongzi1Application {
 
 	public static void main(String[] args) {
-		System.out.print("CCDDDEE");
+		System.out.print("CCDDDEEFF");
 		SpringApplication.run(Dongzi1Application.class, args);
 	}
 }
